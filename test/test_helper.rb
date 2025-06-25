9124d1a1d5329895534b07b185c9095972a2826c
Paulo Arruda <parrudaj@gmail.com>
2025-06-25 10:36:31 -0300

    update test helper and gemfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "llm_mcp"
 
 require "minitest/autorun"
-require "webmock/minitest"
+begin
+  require "webmock/minitest"
+rescue LoadError
+  # WebMock is optional for some tests
+end
 require "tmpdir"
